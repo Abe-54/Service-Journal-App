3358e992e81ee06b6466326ac8ec946deda40669
Abraham Rubio <abraham_rubio@bloomfield.edu>
2023-06-12 21:33:16 -0400

    user can create an account, but screen does not change yet

--- src/api.ts
+++ src/api.ts
@@ -1,55 +1,89 @@
 import { API_BASE_URL } from "@env";
 import axios from "axios";
+import { UserCredential } from "firebase/auth";
 
-const invoiceTrackerURL = axios.create({
+const serviceJournalURL = axios.create({
   baseURL: API_BASE_URL,
 });
 
+export const createNewUser = async (
+  userCredentials: string,
+  userId: string,
+  user_name: string,
+  company_name: string
+) => {
+  const res = await serviceJournalURL.post(
+    "/create/user",
+    {
+      user_id: userId,
+      user_name: user_name,
+      company_name: company_name,
+    },
+    { headers: { Authorization: `Bearer ${userCredentials}` } }
+  );
+  console.log(res.data);
+  return res.data;
+};
+
 export const getClients = async (id: string) => {
-  const res = await invoiceTrackerURL.get(`${id}/clients/`);
+  const res = await serviceJournalURL.get(`${id}/clients/`);
   console.log(res.data);
   return res.data;
 };
 
 export const getSingleClient = async (userId: string, clientId: string) => {
-  const res = await invoiceTrackerURL.get(`${userId}/clients/${clientId}`);
+  const res = await serviceJournalURL.get(`${userId}/clients/${clientId}`);
   console.log(res.data);
   return res.data;
 };
 
 export const getClientJournal = async (userId: string, clientId: string) => {
-  const res = await invoiceTrackerURL.get(`${userId}/journal/client=${clientId}`);
+  const res = await serviceJournalURL.get(
+    `${userId}/journal/client=${clientId}`
+  );
   console.log(res.data);
   return res.data;
 };
 
-export const getJournalEntry = async (userId: string, clientId: string, journalEntryId: string) => {
-  const res = await invoiceTrackerURL.get(`${userId}/journal/client=${clientId}/journal_entry=${journalEntryId}`);
+export const getJournalEntry = async (
+  userId: string,
+  clientId: string,
+  journalEntryId: string
+) => {
+  const res = await serviceJournalURL.get(
+    `${userId}/journal/client=${clientId}/journal_entry=${journalEntryId}`
+  );
   console.log(res.data);
   return res.data;
 };
 
 export const updateJournalEntry = async (journalEntryId: string, data: any) => {
-  const res = await invoiceTrackerURL.patch(`/update/journal_entry=${journalEntryId}`, data);
+  const res = await serviceJournalURL.patch(
+    `/update/journal_entry=${journalEntryId}`,
+    data
+  );
   console.log("New Journal Data: ", res.data);
   return res.data;
 };
 
 export const getAllServices = async (userId: string) => {
-  const res = await invoiceTrackerURL.get(`${userId}/services/`);
+  const res = await serviceJournalURL.get(`${userId}/services/`);
   console.log(res.data);
   return res.data;
-}
+};
 
 export const getServiceById = async (userId: string, service_id: number) => {
-  const res = await invoiceTrackerURL.get(`${userId}/services/service_id=${service_id}`);
-  console.log(res.data); 
+  const res = await serviceJournalURL.get(
+    `${userId}/services/service_id=${service_id}`
+  );
+  console.log(res.data);
   return res.data;
 };
 
 export const getServiceByName = async (userId: string, serviceName: string) => {
-  const res = await invoiceTrackerURL.get(`${userId}/services/service_name=${serviceName}`);
-  console.log(res.data); 
+  const res = await serviceJournalURL.get(
+    `${userId}/services/service_name=${serviceName}`
+  );
+  console.log(res.data);
   return res.data;
 };
-
